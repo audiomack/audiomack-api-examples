c0944655b15f0cc70f1f93e7cd49aa1f385e4688
Glen Scott <glen@glenscott.co.uk>
2018-04-11 12:08:47 +0100

    Updated to prod hostnames

--- php-auth/config.php
+++ php-auth/config.php
@@ -1,9 +1,9 @@
 <?php
 
-$apiUrl         = 'https://audiomack.test/v1';
-$authorizeUrl   = 'https://react.audiomack.test/oauth/authenticate';
-$consumerKey    = 'crowdedhouse';
-$consumerSecret = '2749ba2f1d885e61e7b801bc14b8277e';
+$apiUrl         = 'https://www.audiomack.com/v1';
+$authorizeUrl   = 'https://react.audiomack.com/oauth/authenticate';
+$consumerKey    = 'consumerKey';
+$consumerSecret = 'consumerSecret';
 
 $httpRequest = new HTTP_Request2(null, null, array('ssl_verify_peer' => false));
 
