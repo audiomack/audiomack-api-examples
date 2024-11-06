7708e6112faef904fb1a17018efd2c1fbb471bb1
Glen Scott <glen@glenscott.co.uk>
2024-11-06 09:43:50 +0000

    Add callback to config

--- php-auth/config.php
+++ php-auth/config.php
@@ -4,6 +4,7 @@ $apiUrl         = 'https://api.audiomack.com/v1';
 $authorizeUrl   = 'https://audiomack.com/oauth/authenticate';
 $consumerKey    = 'consumerKey';
 $consumerSecret = 'consumerSecret';
+$callback       = 'http://localhost:8080/callback.php';
 
 $httpRequest = new HTTP_Request2(null, null, array('ssl_verify_peer' => false));
 
